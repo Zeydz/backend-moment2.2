d682f8d5fde9bf5563c24a3ebac503c70fd61335
Jocke <joakim03moller@gmail.com>
2024-04-09 18:07:54 +0200

    Lagt till formulär, styling och GET

---
+++ src/js/main.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/* Kör fetchWorkExperiences när sidan laddats in. */
+document.addEventListener('DOMContentLoaded', () => {
+    fetchWorkExperiences();
+});
+
+/* Hämtar all data från api/work-experiences */
+async function fetchWorkExperiences() {
+    try {
+        const response = await fetch ('https://backend-moment2.onrender.com/api/work-experiences');
+        const data = await response.json();
+
+        if (!response.ok) {
+            throw new Error(data.message) || 'Failed to fetch work experiences.'
+        }
+
+        const workExperiencesList = document.getElementById('work-experiences-list');
+        workExperiencesList.innerHTML = '';
+
+        data.forEach(workExperience => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${workExperience.companyname} - ${workExperience.jobtitle}`;
+            workExperiencesList.appendChild(listItem);
+        })
+
+    } catch (error) {
+        console.error('Error fetching work experiences: ' + error.message);
+    }
+}
